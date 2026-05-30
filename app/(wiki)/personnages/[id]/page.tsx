275f2bc77efd01970609b32c415156cd13ed275d
Cassidy <cassidy.rouelle@gmail.com>
2026-05-30 21:30:02 +0200

    remove layout page perso

--- app/(wiki)/personnages/[id]/page.tsx
+++ app/(wiki)/personnages/[id]/page.tsx
@@ -1,5 +1,4 @@
 import CharacterDetailShell from "@/components/wiki/CharacterDetailShell";
-import WikiLayout from "@/components/wiki/WikiLayout";
 import { getWikiData } from "@/lib/wiki-data";
 import { notFound } from "next/navigation";
 
@@ -9,7 +8,7 @@ type Props = {
 
 export default async function CharacterPage({ params }: Props) {
   const { id } = await params;
-  const { characters, versions, counts, players } = await getWikiData();
+  const { characters } = await getWikiData();
 
   const character = characters.find((entry) => entry.id === id);
 
@@ -17,21 +16,7 @@ export default async function CharacterPage({ params }: Props) {
     notFound();
   }
 
-  const totalRels = characters.reduce(
-    (acc, entry) => acc + (entry.relations?.length || 0),
-    0,
-  );
-
   return (
-    <WikiLayout
-      totalChars={characters.length}
-      totalPlayers={players.length}
-      totalVersions={versions.length}
-      versions={versions}
-      counts={counts}
-      totalRels={totalRels}
-    >
-      <CharacterDetailShell character={character} allCharacters={characters} />
-    </WikiLayout>
+    <CharacterDetailShell character={character} allCharacters={characters} />
   );
 }
