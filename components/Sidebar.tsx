0bcd67e6a91ae2fd7c325ca52a9f28dce81a0782
Cass <cassidy.rouelle@gmail.com>
2026-08-18 15:43:14 +0200

    Update footer text in Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -124,7 +124,7 @@ export default function Sidebar({ open, onClose }: Props) {
       <div className="absolute bottom-0 left-0 w-full mb-10 px-3">
         <AdminButton />
         <p className="text-[10px] text-text-faint text-center mb-2">
-          © {new Date().getFullYear()} casl0x
+          ©{new Date().getFullYear()} - Made by casl0x
         </p>
       </div>
     </div>
